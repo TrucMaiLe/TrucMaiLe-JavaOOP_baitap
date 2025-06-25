67066193a33216b6cf32d3c3da243956f90b3153
TrucMaiLe <tieuminmin296@gmail.com>
2025-06-25 13:22:54 +0700

    Change code bbai 2

--- src/JAVAOOP_BAI2/company/Company.java
+++ src/JAVAOOP_BAI2/company/Company.java
@@ -4,6 +4,13 @@ import JAVAOOP_BAI2.person.Person;
 
 public class Company {
     public static void main(String[] args) {
-        Person person=new Person("Mai",26,"Đà Lạt");
+        Person person=new Person("Mai", 25, "Nữ","Đà Lạt","123456");
+        System.out.println("Name: " +person.name);
+        System.out.println("Age: " +person.age);
+        System.out.println("Gender: " +person.gender);
+//        System.out.println("Adress: " +person.adress);//ko phai public nen ko goi dc
+//        System.out.println("Phone: " +person.phone);//ko phai public nen ko goi dc
+
+
     }
 }
--- src/JAVAOOP_BAI2/person/Person.java
+++ src/JAVAOOP_BAI2/person/Person.java
@@ -1,9 +1,9 @@
 package JAVAOOP_BAI2.person;
 
 public class Person {
-     String name;
-     int age;
-     String gender;
+     public String name;
+    public  int age;
+    public   String gender;
      String adress;
      String phone;
     public Person(String name, int age, String gender,String adress, String phone){
@@ -12,23 +12,10 @@ public class Person {
         this.gender=gender;
         this.adress=adress;
         this.phone=phone;
-        //in ra thông tin
-        System.out.println("Name: " +name);
-        System.out.println("Age: " +age);
-        System.out.println("Gender: " +gender);
-        System.out.println("Adress: " +adress);
-        System.out.println("Phone: " +phone);
+
     }
-    public Person(String name, int age, String gender){
-        this.name=name;
-        this.age=age;
-        this.gender=gender;
-        //In thông tin
-        System.out.println("Name: " +name);
-        System.out.println("Age: " +age);
-        System.out.println("Gender: " +gender);
-    }
-     void getInfo(String name, int age, String gender,String adress, String phone){
+
+    public void getInfo(String name, int age, String gender,String adress, String phone){
         System.out.println("Họ và tên: " +name);
         System.out.println("Tuổi: " +age);
         System.out.println("Giới tính: " +gender);
